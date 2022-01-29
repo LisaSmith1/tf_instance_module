ab1f6ed63c7f95c9c5d022d507d199d649523c57
Lisa <lisasmith@Lisa.local>
2022-01-29 14:51:43 -0800

    Added user_data

--- instance.tf
+++ instance.tf
@@ -15,11 +15,66 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
-  key_name      = var.key
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t3.micro"
+  key_name        = var.key
+  user_data       = <<-EOT
+#cloud-config
+#
+# This is an example file to automatically configure resolv.conf when the
+# instance boots for the first time.
+#
+# Ensure that your yaml is valid and pass this as user-data when starting
+# the instance. Also be sure that your cloud.cfg file includes this
+# configuration module in the appropriate section.
+#
+#Downloads the apache and golang
+packages:
+ - apache2
+ - golang-go
+ - git
+
+runcmd:
+ - mkdir /home/ubuntu/go
+ - export GOPATH=/home/ubuntu/go
+ - go get github.com/hashicorp/lear-go-webapp-demo
+EOT
+  security_groups = [aws_security_group.allow_tls.name]
 
   tags = {
     Name = "${var.env}"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-dd1c28b8"
+
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "s22-cit481"
   }
 }
